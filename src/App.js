76dc827a302289562edbe8cd94624587bdd485c7
Ranvijay Kumar Singh H <ranvijay.kumar@presto-apps.com>
2022-10-23 07:49:14 +0530

    Completed the Fucnctionalty on the Screen'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import MidArea from "./components/MidArea";
 import PreviewArea from "./components/PreviewArea";
 
 export default function App() {
+  const [rotate15Right, setRotate15Right] = useState(0);
+  const [rotate15Left, setRotate15Left] = useState(0);
+  const [move10Steps, setMove10Steps] = useState(0);
+
+  const onClickFlag = () => {
+    console.log("FLag Clicked");
+  };
+  const onClickSprite = () => {
+    console.log("Sprint Clicked ");
+  };
+  const onClickmove10Steps = steps => {
+    if (move10Steps >= 100) setMove10Steps(0);
+    else setMove10Steps(move10Steps + steps);
+  };
+  const onClickTurn15DegRight = deg => {
+    if (rotate15Right >= 360) setRotate15Right(0);
+    else setRotate15Right(rotate15Right + deg);
+  };
+  const onClickTurn15DegLeft = deg => {
+    if (rotate15Left >= 360) setRotate15Left(0);
+    else setRotate15Left(rotate15Left + deg);
+  };
+
   return (
     <div className="bg-blue-100 pt-6 font-sans">
       <div className="h-screen overflow-hidden flex flex-row  ">
         <div className="flex-1 h-screen overflow-hidden flex flex-row bg-white border-t border-r border-gray-200 rounded-tr-xl mr-2">
-          <Sidebar /> <MidArea />
+          <Sidebar
+            onClickFlag={onClickFlag}
+            onClickSprite={onClickSprite}
+            onClickmove10Steps={onClickmove10Steps}
+            onClickTurn15DegRight={onClickTurn15DegRight}
+            onClickTurn15DegLeft={onClickTurn15DegLeft}
+          />{" "}
+          <MidArea />
         </div>
         <div className="w-1/3 h-screen overflow-hidden flex flex-row bg-white border-t border-l border-gray-200 rounded-tl-xl ml-2">
-          <PreviewArea />
+          <PreviewArea
+            moveSteps={move10Steps}
+            rotateRight={rotate15Right}
+            rotateLeft={rotate15Left}
+          />
         </div>
       </div>
     </div>
--- src/components/PreviewArea.js
+++ src/components/PreviewArea.js
@@ -1,10 +1,24 @@
 import React from "react";
 import CatSprite from "./CatSprite";
 
-export default function PreviewArea() {
+export default function PreviewArea(props) {
+  const { moveSteps, rotateRight, rotateLeft } = props || {};
+  console.log("move Steps==>", moveSteps);
+  console.log("rotateRight==>", rotateRight);
+  console.log("rotateLeft==>", rotateLeft);
   return (
-    <div className="flex-none h-full overflow-y-auto p-2">
-      <CatSprite />
+    <div className={`flex-none h-1/2 w-11/12 overflow-y-auto p-2 `}>
+      <div className=" h-full w-full flex content-center items-center justify-center">
+        <div
+          className={` h-35 w-35 ${moveSteps ? `ml-${moveSteps}` : ""} ${
+            rotateLeft ? `transform origin-center -rotate-${rotateLeft}` : ""
+          }  ${
+            rotateRight ? `transform origin-center rotate-${rotateRight}` : ""
+          }`}
+        >
+          <CatSprite />
+        </div>
+      </div>
     </div>
   );
 }
